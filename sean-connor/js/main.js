19a8bad88adda503a754a26eb4de7a3a94911821
Sean Connor <connorseanp@gmail.com>
2016-04-08 11:24:25 -0700

    complete mvp for AKQA

--- sean-connor/js/main.js
+++ sean-connor/js/main.js
@@ -1,13 +1,14 @@
-function expandMenu() {
+function expandMenu(event) {
     document.getElementsByClassName("nav-list")[0].classList.toggle("display");
+    event.target.classList.toggle("menu-color");
 }
 
 function expandSongs(event) {
-  songs = document.getElementsByClassName("aside")[0].children
+  var songs = document.getElementsByClassName("aside")[0].children
   for (var i = 2; i < songs.length; i++) {
     if (songs[i].classList[0] === "song-group" && songs[i].classList[1] !== "top-3"){
     songs[i].classList.toggle("song-display");
     }
   }
-  (event.target.innerHTML === "⌄") ? event.target.innerHTML = "^" : event.target.innerHTML = "⌄"
+  (event.target.innerHTML === "∨") ? event.target.innerHTML = "∧" : event.target.innerHTML = "∨"
 }
